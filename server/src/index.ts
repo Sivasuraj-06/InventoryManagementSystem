55d941d87adb92f97adf65cf23657991985dc4fd
sivasubramaniyanr <rajkumarsubramani63@gmail.com>
2026-04-03 14:18:20 +0530

    modify server/src/index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,6 @@ import productRoutes from "./routes/productRoutes";
 import userRoutes from "./routes/userRoutes";
 import expenseRoutes from "./routes/expenseRoutes";
 
-
 /* CONFIGURATIONS */
 dotenv.config();
 const app = express();
@@ -23,12 +22,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 /* ROUTES */
-app.use("/dashboard",dashboardRoutes); 
-app.use("/products",productRoutes);
-app.use("/users",userRoutes);
-app.use("/expenses",expenseRoutes);
+app.use("/dashboard", dashboardRoutes);
+app.use("/products", productRoutes);
+app.use("/users", userRoutes);
+app.use("/expenses", expenseRoutes);
 /* SERVER */
-const port = process.env.PORT || 3001;
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`);
+const port = Number(process.env.PORT || 3001);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server is running on port ${port}`);
 });
